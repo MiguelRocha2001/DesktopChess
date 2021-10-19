9a50db4cafb4eadb481d7ee8141e1987e2326de4
MiguelRocha2001 <migasrocha1@hotmail.com>
2021-10-19 22:34:47 +0100

    Board type in working phase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 enum class PieceType() {
-    K, Q, B, N, R, P
+    K, Q, B, N, R, P, k, q, b, n, r, p
 }
 
 private class Piece(val type: PieceType, val line: Int, val col: Char)
 
 class Board {
-    private val board = HashSet<Piece>()
+    private val board = HashMap<Pair<Int,Char>,Piece>()
 
     constructor() {
 
@@ -13,10 +13,10 @@ class Board {
 
     private fun init() {
         initPlayer1()
-
+        initPlayer2()
     }
 
-    private fun initPlayer1() {
+    private fun initPlayer2() {
         addPiece(PieceType.R, 1, 'a')
         addPiece(PieceType.N, 1, 'b')
         addPiece(PieceType.B, 1, 'c')
@@ -30,23 +30,23 @@ class Board {
         }
     }
 
-    private fun initPlayer2() {
-        addPiece(PieceType.R, 8, 'a')
-        addPiece(PieceType.N, 8, 'b')
-        addPiece(PieceType.B, 8, 'c')
-        addPiece(PieceType.Q, 8, 'd')
-        addPiece(PieceType.K, 8, 'e')
-        addPiece(PieceType.B, 8, 'f')
-        addPiece(PieceType.N, 8, 'g')
-        addPiece(PieceType.R, 8, 'h')
+    private fun initPlayer1() {
+        addPiece(PieceType.r, 8, 'a')
+        addPiece(PieceType.n, 8, 'b')
+        addPiece(PieceType.b, 8, 'c')
+        addPiece(PieceType.q, 8, 'd')
+        addPiece(PieceType.k, 8, 'e')
+        addPiece(PieceType.b, 8, 'f')
+        addPiece(PieceType.n, 8, 'g')
+        addPiece(PieceType.r, 8, 'h')
         for (i in 1..8) {
-            addPiece(PieceType.P, 7, 'a' + (i - 1))
+            addPiece(PieceType.p, 7, 'a' + (i - 1))
         }
     }
 
 
     private fun addPiece(type: PieceType, line: Int, col: Char) {
-        board.add(createPiece(type,line,col))
+        board.add(Pair(line,col), createPiece(type,line,col))
     }
 
 
@@ -54,7 +54,13 @@ class Board {
 
 
     override fun toString(): String {
-        return super.toString()
+        val str = ""
+        val iterator = board.iterator()
+        var aux
+        while (iterator.hasNext()) {
+            aux = iterator.next()
+            str += iterator.next().type.toString()
+        }
     }
 }
 
